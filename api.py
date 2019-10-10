f95883a0cb0254651c29ea508f86dc8cbfb7842b
Simon-j-Cobb <simoncobb@msn.com>
2019-10-10 12:15:04 +0100

    plz

--- api.py
+++ api.py
@@ -1,16 +1,39 @@
 from flask import Flask, request
-from flask_cors import CORS
-import requests
-# from SelectDAO import SelectDAO
+import json
+from SelectDAO import SelectDAO
 # from InsertDAO import InsertDAO
 
+
 app = Flask(__name__)
-CORS(app)
+selector = SelectDAO()
 
 @app.route("/", methods=['POST', 'GET'])
 def result():
     return 'post req'
 
+
+@app.route("/get_companies", methods=['GET'])
+def get():
+     with open('data/companiess.json') as f:
+          data = json.load(f)
+          return json.dumps(data)
+
+@app.route("/get_all_user", methods=['GET'])
+def get_all():
+    selected = selector.select_user('001')
+    return json.dumps(selected)
+
+@app.route("/get_companies/", methods=['GET'])
+def get_agg_comp():
+    user = request.args.get('user')
+    selected = selector.select_comp_agg(user)
+    return selected
+
+@app.route("/get_charities/", methods=['GET'])
+def get_agg_char():
+    user = request.args.get('user')
+    selected = selector.select_char_agg(user)
+    return selected
 
 @app.route("/check_connection", methods=['POST', 'GET'])
 def check_connection():
@@ -22,5 +45,4 @@ def bootapp():
 
 if __name__ == '__main__':
      bootapp()
-
 
